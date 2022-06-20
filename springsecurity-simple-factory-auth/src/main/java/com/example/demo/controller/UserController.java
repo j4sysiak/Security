1f5dfef75eca4d544bbdfdfd3efe5c9633ab304f
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-20 18:00:49 +0200

    20-06-2022 17:17

--- springsecurity-simple-factory-auth/src/main/java/com/example/demo/controller/UserController.java
+++ springsecurity-simple-factory-auth/src/main/java/com/example/demo/controller/UserController.java
@@ -4,10 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloApiController {
+public class UserController {
 
     @RequestMapping("/hello")
     public String get() {
         return "hello";
+    }
+
+    @RequestMapping("/for-admin")
+    public String forAdmin() {
+        return "hello-admin";
+    }
+
+    @RequestMapping("/for-user")
+    public String forUser() {
+        return "hello-user";
     }
 }
