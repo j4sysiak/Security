54a9bb0d33fc3a715d6ca9d8ce460e6a9e9af450
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-20 07:01:37 +0200

    20-06-2022 06:27

--- springsecurity-simple-factory-auth/src/main/java/com/example/demo/model/AppUser.java
+++ springsecurity-simple-factory-auth/src/main/java/com/example/demo/model/AppUser.java
@@ -2,19 +2,58 @@ package com.example.demo.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
-@Getter
-@Setter
-public class AppUser {
+//@Getter
+//@Setter
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
     private String userName;
-
     private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
